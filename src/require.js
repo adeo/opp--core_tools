41af65dbe7935f061b7a72ca28f3bea2b4781e69
llevkin <leonid.levkin@leroymerlin.ru>
2019-05-21 15:32:26 +0300

    require

--- src/require.js
+++ src/require.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports.folder = dir => {
-    let result = {}, names = fs.readdirSync(dir);
+module.exports.folder = (dir, exclude = []) => {
+    let field, result = {}, names = fs.readdirSync(dir);
     for (let name of names) {
         if (path.extname(name) !== '.js')
             continue;
-        result[path.basename(name, '.js')] = require(path.normalize(path.join(dir, name)));
+        field = path.basename(name, '.js');
+        if (exclude.includes(field))
+            continue;
+        result[field] = require(path.normalize(path.join(dir, name)));
     }
     return result;
 };
