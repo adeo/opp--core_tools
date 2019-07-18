29aa513919da4c905e0bb23b36157a412cfb2fa8
llevkin <leonid.levkin@leroymerlin.ru>
2019-07-18 12:12:22 +0300

    require.folder recursive flag

--- src/require.js
+++ src/require.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports.folder = (dir, exclude = [], result = {}) => {
+module.exports.folder = (dir, exclude = [], result = {}, recursive = true) => {
     let stat, field, names = fs.readdirSync(dir);
     for (let name of names) {
         stat = fs.statSync(`${dir}/${name}`);
-        if (stat.isDirectory()) {
+        if (stat.isDirectory() && recursive) {
             result[name] = {};
             module.exports.folder(`${dir}/${name}`, exclude, result[name]);
             continue;
